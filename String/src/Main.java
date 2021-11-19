e1ea289f5a357220c7de4689e6f3afdd791480cd
Johnsonnen <65743146+Johnsonnen@users.noreply.github.com>
2021-11-18 23:38:12 -0800

    Add files via upload

---
+++ String/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("      __...--~~~~~-._   _.-~~~~~--...__");
+        System.out.println("    //               `V'               \\\\       ,--./,--.");
+        System.out.println("   //                 |                 \\\\     / #       \\");
+        System.out.println("  //__...--~~~~~~-._  |  _.-~~~~~~--...__\\\\   |           |");
+        System.out.println(" //__.....----~~~~._\\ | /_.~~~~----.....__\\\\   \\         / ");
+        System.out.println("====================\\\\|//====================   `._,.._,'");
+        System.out.println("                    `---`");
+    }
+}
+
